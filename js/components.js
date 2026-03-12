dd1f5e145ed889154d9ae8d2584d928e6eaf0c9c
juandy412 <jdgarciaq@ufpso.edu.co>
2026-03-11 20:19:46 -0500

    proyecto de componentes

---
+++ js/components.js
@@ -0,0 +1,31 @@
+fetch("../mini-catalogo/components/header/header.html")
+.then(res => res.text())
+.then(data => {
+    document.getElementById("header").innerHTML = data;
+});
+
+fetch("../mini-catalogo/components/footer/footer.html")
+.then(res => res.text())
+.then(data => {
+    document.getElementById("footer").innerHTML = data;
+});
+class ProductCard extends HTMLElement {
+
+    connectedCallback(){
+
+        const nombre = this.getAttribute("nombre");
+        const precio = this.getAttribute("precio");
+        const descripcion = this.getAttribute("descripcion");
+
+        this.innerHTML = `
+        <div class="card">
+            <h3>${nombre}</h3>
+            <p>${descripcion}</p>
+            <span>$${precio}</span>
+        </div>
+        `;
+    }
+
+}
+
+customElements.define("product-card", ProductCard);
